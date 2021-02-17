29f337e6496fa26d2f1bc2cd0991ee059219b39b
Ryan Moran <rmoran@vmware.com>
2021-02-17 15:17:46 -0800

    Update CacheVolumeNames generation to support pack 0.17

--- cache_volume_names.go
+++ cache_volume_names.go
@@ -3,16 +3,22 @@ package occam
 import (
 	"crypto/sha256"
 	"fmt"
+	"strings"
 )
 
 func CacheVolumeNames(name string) []string {
 	refName := []byte(fmt.Sprintf("%s:latest", name))
-
 	sum := sha256.Sum256(refName)
+
+	parts := strings.SplitN(name, "/", 2)
+	if len(parts) == 2 {
+		name = parts[1]
+	}
 
 	var volumes []string
 	for _, t := range []string{"build", "launch", "cache"} {
 		volumes = append(volumes, fmt.Sprintf("pack-cache-%x.%s", sum[:6], t))
+		volumes = append(volumes, fmt.Sprintf("pack-cache-%s_latest-%x.%s", name, sum[:6], t))
 	}
 
 	return volumes
--- cache_volume_names_test.go
+++ cache_volume_names_test.go
@@ -15,8 +15,24 @@ func testCacheVolumeNames(t *testing.T, context spec.G, it spec.S) {
 	it("returns the name of the cache volumes that are assigned to an image", func() {
 		Expect(occam.CacheVolumeNames("some-app")).To(Equal([]string{
 			"pack-cache-16fe664c76f0.build",
+			"pack-cache-some-app_latest-16fe664c76f0.build",
 			"pack-cache-16fe664c76f0.launch",
+			"pack-cache-some-app_latest-16fe664c76f0.launch",
 			"pack-cache-16fe664c76f0.cache",
+			"pack-cache-some-app_latest-16fe664c76f0.cache",
 		}))
+	})
+
+	context("when the name includes a registry", func() {
+		it("returns the name of the cache volumes that are assigned to an image", func() {
+			Expect(occam.CacheVolumeNames("occam.example.com/some-app")).To(Equal([]string{
+				"pack-cache-e69d3a4f1e12.build",
+				"pack-cache-some-app_latest-e69d3a4f1e12.build",
+				"pack-cache-e69d3a4f1e12.launch",
+				"pack-cache-some-app_latest-e69d3a4f1e12.launch",
+				"pack-cache-e69d3a4f1e12.cache",
+				"pack-cache-some-app_latest-e69d3a4f1e12.cache",
+			}))
+		})
 	})
 }
