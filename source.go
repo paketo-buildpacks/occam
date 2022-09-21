41c87e95af9026724611103d24875633851cb08a
Joshua Casey <joshuatcasey@gmail.com>
2022-09-20 19:53:32 -0500

    Add comments to Source

--- source.go
+++ source.go
@@ -8,9 +8,14 @@ import (
 	"github.com/paketo-buildpacks/packit/v2/fs"
 )
 
+// Source will copy `path` into a temporary directory and return the path to the temporary directory.
+// It will also place a file with random contents into the temporary directory, to ensure that the
+// contents are globally unique, which is meant to bypass reuse of cached layers.
+//
+// The caller must clean up the returned directory.
 func Source(path string) (string, error) {
 	destination, err := os.MkdirTemp("", "source")
-	if err != nil {
+	if err != nil { // untested
 		return "", err
 	}
 
@@ -21,12 +26,12 @@ func Source(path string) (string, error) {
 
 	content := make([]byte, 32)
 	_, err = rand.Read(content)
-	if err != nil {
+	if err != nil { // untested
 		return "", err
 	}
 
 	err = os.WriteFile(filepath.Join(destination, ".occam-key"), content, 0644)
-	if err != nil {
+	if err != nil { // untested
 		return "", err
 	}
 
