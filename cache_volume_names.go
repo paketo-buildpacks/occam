c965280f6e11dc1ecb0a5d2654ec2701bef9cd76
Costas Papastathis <papastathiscr@gmail.com>
2026-01-05 20:18:53 +0200

    fix: removing kaniko volume cache (#450)

--- cache_volume_names.go
+++ cache_volume_names.go
@@ -6,11 +6,12 @@ import (
 	"strings"
 )
 
-func CacheVolumeNames(name string) []string {
+func CacheVolumeNames(volumeName string) []string {
+	name := volumeName
 	refName := []byte(fmt.Sprintf("%s:latest", name))
 	sum := sha256.Sum256(refName)
 
-	parts := strings.SplitN(name, "/", 2)
+	parts := strings.SplitN(volumeName, "/", 2)
 	if len(parts) == 2 {
 		name = parts[1]
 	}
@@ -20,6 +21,10 @@ func CacheVolumeNames(name string) []string {
 		volumes = append(volumes, fmt.Sprintf("pack-cache-%x.%s", sum[:6], t))
 		volumes = append(volumes, fmt.Sprintf("pack-cache-%s_latest-%x.%s", name, sum[:6], t))
 	}
+
+	kanikoRefName := []byte(fmt.Sprintf("%s%s-volume", refName, volumeName))
+	kanikoSum := sha256.Sum256(kanikoRefName)
+	volumes = append(volumes, fmt.Sprintf("pack-cache-%s_latest-%x.kaniko", name, kanikoSum[:6]))
 
 	return volumes
 }
